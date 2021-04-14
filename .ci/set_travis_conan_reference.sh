32a5c7dbbf59a947698055279c6b5c2c4a81d084
bldrvnlw <b.van_lew@lumc.nl>
2021-04-14 11:19:49 +0200

    Add reference and upload logic

---
+++ .ci/set_travis_conan_reference.sh
@@ -0,0 +1,19 @@
+if [ $# -ne 2 ] && [$# -ne 1 ] ; then
+    echo Error in set_travis_reference
+    echo "Usage: set_travis_reference <package_name> (user/channel) "
+    echo sets the environment variable REFERENCE with package_name/version@user/channel
+    echo Where version is derived from the TRAVIS_BRANCH/TRAVIS_TAG
+    exit 1
+fi
+
+REGEX_FEATURE='^feature/(.*)$'
+REGEX_MASTER='^master$'
+
+if [ ${#TRAVIS_TAG} gt 0 ] then; export CONAN_REFERENCE=$1/${TRAVIS_TAG}@$2; exit 0; fi
+if [[ $TRAVIS_BRANCH =~ $REGEX_MASTER ]]; export CONAN_REFERENCE=$1/latest@$2; exit 0; fi
+if [[ $TRAVIS_BRANCH =~ $REGEX_FEATURE ]]; export CONAN_REFERENCE=$1/${BASH_REMATCH[1]}@$2; exit 0; fi
+
+echo Error in set_travis_reference
+echo Expected either:
+echo 1)  a TRAVIS_TAG with a version number
+echo 2)  a TRAVIS_BRANCH with "feature/.*" or "master"
