5b35ddc395a204b7855cd6890b3b1feeef983554
B. van Lew <b.van_lew@lumc.nl>
2021-05-18 14:51:15 +0200

    Manually install lz4 on macos

--- .ci/install.sh
+++ .ci/install.sh
@@ -27,3 +27,7 @@ conanhome=`conan config home`
 cat cert.pem >> $conanhome/cacert.pem
 conan remote remove conan-center
 conan remote add center-artifactory https://conan.bintray.com
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    conan install lz4/1.9.2@ -r conan-center -s compiler=apple-clang -s arch=x86_64 -s build_type=Release -s compiler.version=10.0 -s os=Macos -o fPIC=True -o shared=False
+fi
