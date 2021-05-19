290720dde54417de74b67b4dfbbf79910d39e2c4
B. van Lew <b.van_lew@lumc.nl>
2021-05-19 11:01:49 +0200

    Cleanup appveyor file

---
+++ .ci/make_profile.bat
@@ -0,0 +1,7 @@
+  - conan profile new action_build
+  - conan profile update settings.os=Windows action_build
+  - conan profile update settings.os_build=Windows action_build
+  - conan profile update settings.arch=%CONAN_ARCHS% action_build
+  - conan profile update settings.arch_build=%CONAN_ARCHS% action_build
+  - conan profile update settings.compiler="Visual Studio" action_build
+  - conan profile update settings.compiler.version=%CONAN_VISUAL_VERSIONS% action_build
