f1c5a10590620e353ecc9f161ac57d4ee6ffb55a
Baldur van Lew <baldurvanlew@gmail.com>
2021-05-19 14:26:16 +0200

    Test the macOS build on 10.3

--- .ci/conan_dependencies.sh
+++ .ci/conan_dependencies.sh
@@ -8,7 +8,24 @@ conan user
 cd $project && .ci/pemsetup.sh
 conan remote add lkeb-artifactory $CONAN_UPLOAD
 conan remote list
-conan profile new action_build --force --detect && conan profile show action_build
+
+if [[ $TRAVIS_OS_NAME -eq osx ]] ; then
+    CONAN_OS=Macos
+    CONAN_COMPILER=apple-clang
+else
+    CONAN_OS=Linux
+    CONAN_COMPILER=gcc
+fi
+conan profile new action_build --force
+conan profile update settings.os=$CONAN_OS action_build
+conan profile update settings.os_build=$CONAN_OS action_build
+conan profile update settings.arch=x86_64 action_build
+conan profile update settings.arch_build=x86_64 action_build
+conan profile update settings.compiler=$CONAN_COMPILER action_build
+conan profile update settings.compiler.version=$CONAN_COMPILERVERSION action_build
+conan profile update settings.compiler.libcxx=libc++ action_build
+conan profile show action_build
+
 conan install . -s build_type=Release --profile action_build
 conan install . -s build_type=Debug --profile action_build
 cd test_package
