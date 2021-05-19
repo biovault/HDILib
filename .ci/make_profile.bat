4ca96e6816c6a95ca9278cd57b078b3ad0f323bb
B. van Lew <b.van_lew@lumc.nl>
2021-05-19 17:14:38 +0200

    Restore missing arch

--- .ci/make_profile.bat
+++ .ci/make_profile.bat
@@ -1,5 +1,8 @@
 conan profile new action_build
 conan profile update settings.os=Windows action_build
 conan profile update settings.os_build=Windows action_build
+conan profile update settings.arch=x86_64 action_build
+conan profile update settings.arch_build=x86_64 action_build
 conan profile update settings.compiler="Visual Studio" action_build
 conan profile update settings.compiler.version=%CONAN_VISUAL_VERSIONS% action_build
+conan profile show action_build
