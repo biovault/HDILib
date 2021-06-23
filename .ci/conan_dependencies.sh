b7fbb9241d07e6ce8a23abbb0171bc3a59cde84d
bldrvnlw <b.van_lew@lumc.nl>
2021-06-23 14:32:07 +0200

    Debug TRAVIS OS

--- .ci/conan_dependencies.sh
+++ .ci/conan_dependencies.sh
@@ -8,13 +8,16 @@ conan user
 cd $project && .ci/pemsetup.sh
 conan remote add lkeb-artifactory $CONAN_UPLOAD
 conan remote list
+echo TRAVIS OS name $TRAVIS_OS_NAME
 
 if [[ $TRAVIS_OS_NAME -eq osx ]] ; then
     CONAN_OS=Macos
     CONAN_COMPILER=apple-clang
-else
-    CONAN_OS=Linux
-    CONAN_COMPILER=gcc
+fi
+
+if [[ $TRAVIS_OS_NAME -eq linux ]] ; then
+    export CONAN_OS=Linux
+    export CONAN_COMPILER=gcc
 fi
 conan profile new action_build --force
 conan profile update settings.os=$CONAN_OS action_build
