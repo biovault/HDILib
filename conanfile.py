1fa4a78907bea10f5df60754e4cbe8375a0eaa51
bldrvnlw <b.van_lew@lumc.nl>
2021-07-27 12:28:20 +0200

    Switch to github actions and new conan

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
+from conan.tools.cmake import CMakeDeps, CMake, CMakeToolchain
 import os
 import re
 import subprocess
@@ -11,7 +12,7 @@ from pathlib import Path
 
 class HDILibConan(ConanFile):
     name = "HDILib"
-    default_version = "1.0.0-alpha1"
+    version = "1.2.4"
     description = "HDILib is a library for the scalable analysis of large and high-dimensional data. "
     topics = ("embedding", "analysis", "n-dimensional", "tSNE")
     url = "https://github.com/biovault/HDILib"
@@ -20,12 +21,12 @@ class HDILibConan(ConanFile):
     default_user = "lkeb"
     default_channel = "stable"
 
-    generators = ["cmake_multi", "cmake_find_package_multi"]
+    generators = "CMakeDeps"
 
     # Options may need to change depending on the packaged library
     settings = "os", "compiler", "arch", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {"shared": [False]}
+    default_options = {"shared": False}
 
     # scm = {
     #    "type": "git",dir
@@ -33,12 +34,7 @@ class HDILibConan(ConanFile):
     #    "submodule": "recursive"
     # }
     # , "conanbuildinfo.txt", "conanbuildinfo_debug.cmake", "conanbuildinfo_release.cmake", "conanbuildinfo_multi.cmake"
-    exports = "hdi*", "external*", "cmake*", "*.cmake", "CMakeLists.txt", "LICENSE"
-
-    # Flann builds are bit complex and certain versions fail with
-    # certain platform, and compiler combinations. Hence use
-    # either self built 1.8.5 for Windows or system supplied
-    # 1.8.4 on Linux and Macos
+    exports = "hdi*", "external*", "cmake*", "CMakeLists.txt", "LICENSE"
 
     def _get_python_cmake(self):
         if None is not os.environ.get("APPVEYOR", None):
@@ -54,47 +50,71 @@ class HDILibConan(ConanFile):
                 installer = tools.SystemPackageTool()
                 installer.install("libomp")
 
+    # Flann builds are bit complex and certain versions fail with
+    # certain platform, and compiler combinations. Hence use
+    # either self built 1.8.5 for Windows or system supplied
+    # 1.8.4 on Linux and Macos
     def requirements(self):
         if self.settings.build_type == "None":
             print("Skip root package requirements for build_type NONE")
             return
         if self.settings.os == "Windows":
-            self.requires("flann/1.8.5@lkeb/stable")
+            self.requires("flann/1.8.5@lkeb/testing")
         else:
-            # Macos and flann use 1.8.4
-            self.requires("flann/1.8.4@lkeb/stable")
+            # Macos and linux use 1.8.4
+            self.requires("flann/1.8.5@lkeb/testing")
         # self.requires.add("lz4/1.9.2")
 
-    def configure(self):
-        if self.settings.compiler == "Visual Studio":
-            del self.settings.compiler.runtime
+    def fix_config_packages(self):
+        """Iterate the dependencies and add the package root where
+        it is marked as a "cmake_config_file" and when "skip_deps_file" is
+        enabled. Permits using a package local cmake config-file.
+        (the flann package is build with skip-deps)
+        """
+        package_names = {r.ref.name for r in self.dependencies.host.values()}
+        for package_name in package_names:
+            cpp_info = self.dependencies[f"{package_name}"].new_cpp_info
+            if cpp_info.get_property("skip_deps_file", CMakeDeps):
+                print(f"No deps generated for {package_name} - skip_deps_file found")
+            if cpp_info.get_property(
+                "skip_deps_file", CMakeDeps
+            ) and cpp_info.get_property("cmake_config_file", CMakeDeps):
+                package_root = Path(self.dependencies[f"{package_name}"].package_folder)
+                with open("conan_toolchain.cmake", "a") as toolchain:
+                    toolchain.write(
+                        fr"""
+set(CMAKE_MODULE_PATH "{package_root.as_posix()}" ${{CMAKE_MODULE_PATH}})
+set(CMAKE_PREFIX_PATH "{package_root.as_posix()}" ${{CMAKE_PREFIX_PATH}})
+                    """
+                    )
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def _configure_cmake(self, build_type):
-        # Inject the conan dependency paths into the CMakeLists.txt
-        conanproj = (
-            "PROJECT(${PROJECT})\n"
-            "include(${CMAKE_BINARY_DIR}/conanbuildinfo_multi.cmake)\n"
-            "conan_basic_setup()\n"
-        )
-        tools.replace_in_file("CMakeLists.txt", "PROJECT(${PROJECT})", conanproj)
+    def generate(self):
+        print("In generate")
+        generator = None
         if self.settings.os == "Macos":
-            cmake = CMake(self, generator="Xcode", build_type=build_type)
-        else:
-            cmake = CMake(self, build_type=build_type)
+            generator = "Xcode"
+
+        if self.settings.os == "Linux":
+            generator = "Ninja Multi-Config"
+
+        tc = CMakeToolchain(self, generator=generator)
         if self.settings.os == "Windows" and self.options.shared:
-            cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
+            tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
         if self.settings.os == "Linux" or self.settings.os == "Macos":
-            cmake.definitions["CMAKE_CXX_STANDARD"] = 14
-            cmake.definitions["CMAKE_CXX_STANDARD_REQUIRED"] = "ON"
-        cmake.definitions[
-            "HDI_EXTERNAL_FLANN_INCLUDE_DIR"
-        ] = "${CONAN_INCLUDE_DIRS_FLANN}"
-        cmake.definitions["HDI_USE_ROARING"] = "OFF"
-        cmake.definitions["HDILib_VERSION"] = self.version
+            tc.variables["CMAKE_CXX_STANDARD"] = 14
+            tc.variables["CMAKE_CXX_STANDARD_REQUIRED"] = "ON"
+        tc.variables["HDI_EXTERNAL_FLANN_INCLUDE_DIR"] = "${CONAN_INCLUDE_DIRS_FLANN}"
+        tc.variables["HDI_USE_ROARING"] = "OFF"
+        tc.variables["HDILib_VERSION"] = self.version
+        tc.variables["CMAKE_INSTALL_PREFIX"] = str(Path(self.build_folder, "install"))
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+        self.fix_config_packages()
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
         print(f"Set version to {self.version}")
         cmake.configure()
         cmake.verbose = True
@@ -106,53 +126,12 @@ class HDILibConan(ConanFile):
         )
         install_dir = Path(self.build_folder).joinpath("install")
         install_dir.mkdir(exist_ok=True)
-        config = str(self.settings.build_type)
-        print(f"Installing: install for {config} build")
-        cmakepath = self._get_python_cmake()
 
-        cmake_debug = self._configure_cmake("Debug")
-        cmake_debug.build()
-        result = subprocess.run(
-            [
-                f"{str(cmakepath)}",
-                "--install",
-                self.build_folder,
-                "--config",
-                "Debug",
-                "--verbose",
-                "--prefix",
-                str(install_dir),
-            ],
-            capture_output=True,
-        )
+        cmake_debug = self._configure_cmake()
+        cmake_debug.install(build_type="Debug")
 
-        cmake_release = self._configure_cmake("Release")
-        cmake_release.build()
-        result = subprocess.run(
-            [
-                f"{str(cmakepath)}",
-                "--install",
-                self.build_folder,
-                "--config",
-                "Release",
-                "--verbose",
-                "--prefix",
-                str(install_dir),
-            ],
-            capture_output=True,
-        )
-
-        print(
-            f"Install for {config} build - complete. \n Output: {result.stdout} \n Errors: {result.stderr}"
-        )
-
-    def package(self):
-        install_dir = Path(self.build_folder).joinpath("install")
-        self.copy(pattern="*", src=str(install_dir))
-        # Add the debug support files to the package
-        # (*.pdb) if building the Visual Studio version
-        if self.settings.compiler == "Visual Studio":
-            self.copy("*.pdb", dst="lib/Debug", keep_path=False)
+        cmake_release = self._configure_cmake()
+        cmake_release.install(build_type="Release")
 
     def package_id(self):
         # The package contains both Debug and Release build types
@@ -163,3 +142,13 @@ class HDILibConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.set_property("cmake_file_name", "ConanHDILib")
+        self.cpp_info.set_property("skip_deps_file", True)
+        self.cpp_info.set_property("cmake_config_file", True)
+
+    def package(self):
+        install_dir = Path(self.build_folder).joinpath("install")
+        self.copy(pattern="*", src=str(install_dir))
+        # Add the debug support files to the package
+        # (*.pdb) if building the Visual Studio version
+        if self.settings.compiler == "Visual Studio":
+            self.copy("*.pdb", dst="lib/Debug", keep_path=False)
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,26 +1,28 @@
 import os
 import platform
 from pathlib import Path
-import pathlib
-
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 
 # This is a "hello world" type test that checks that the conan package can be consumed
 # i.e. that that cmake support works, consumption of HDILib headers (compiler) and lib (linker) works
 # and nothing else. It is is not a full unit or regression test for HDILib.
+
 
 class HDILibTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [False]}
     # cmake_paths locates the HDILib built with build_type None
     # cmake_find_package creates the lz4Targets.cmake locating - only Release needs to be configured
-    generators = "cmake_find_package_multi", "cmake_paths"
+    generators = "CMakeDeps"
 
-    def configure(self):
-        print("In configure")
-
-    def config_options(self):
-        print("In config_options")
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["HDILib_ROOT"] = self.deps_cpp_info["HDILib"].rootpath
+        tc.variables["flann_ROOT"] = self.deps_cpp_info["flann"].rootpath
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def requirements(self):
         print("In requirements")
@@ -28,7 +30,9 @@ class HDILibTestConan(ConanFile):
             print("Skip test_package requirements for build_type NONE")
             return
         else:
-            print(f"Setting test_package requirements for build-type {self.settings.build_type }")
+            print(
+                f"Setting test_package requirements for build-type {self.settings.build_type}"
+            )
             self.requires.add("lz4/1.9.2")
 
     def system_requirements(self):
@@ -38,24 +42,18 @@ class HDILibTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        if self.settings.build_type == "None":
-            print("Test consumption of HDILib in Release mode")
-            self.settings.build_type = "Release"
-            cmake = CMake(self, build_type="Release")
-        print(f"")
-        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = "conan_paths.cmake"
         cmake.configure()
         cmake.build()
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
         self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
+        self.copy("*.so*", dst="bin", src="lib")
 
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
-            if platform.system() == 'Windows':
+            if platform.system() == "Windows":
                 examplePath = Path("./", str(self.build_folder), "bin", "example.exe")
                 self.run(f"{str(examplePath)}")
             else:
