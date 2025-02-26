49aac159d37349fa612433184eb379b5d895c06d
Alexander Vieth <vieth.alexander@gmx.net>
2025-02-26 10:14:29 +0100

    update version 1.2.10

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ required_conan_version = "~=1.66.0"
 
 class HDILibConan(ConanFile):
     name = "HDILib"
-    version = "latest"
+    version = "1.2.10"
     description = "HDILib is a library for the scalable analysis of large and high-dimensional data. "
     topics = ("embedding", "analysis", "n-dimensional", "tSNE")
     url = "https://github.com/biovault/HDILib"
