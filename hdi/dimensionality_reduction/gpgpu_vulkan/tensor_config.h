c7d422b7fc9bf59cb41b030a9e612d0ff11f813e
Baldur van Lew <b.van_lew@lumc.nl>
2025-10-14 17:02:31 +0200

    Define UBO parameters and tensors as needed for shaders

--- hdi/dimensionality_reduction/gpgpu_vulkan/tensor_config.h
+++ hdi/dimensionality_reduction/gpgpu_vulkan/tensor_config.h
@@ -4,6 +4,42 @@
 #include <memory>
 #include <kompute/Kompute.hpp>
 
+
+// The UBO structs used to create the buffers
+struct stencilParams {
+  float rect_min[2];
+  float rect_max[2];  
+  float image_size[2];
+};
+
+struct fieldParams {
+  float image_size[2];
+  float support;
+};
+
+struct interpParams {
+  float image_size[2];
+};
+
+struct forcesParams {
+  float exaggeration;
+};
+
+struct updaterParams {
+  float eta;
+  float minGain;
+  float iter;
+  float mom_iter;
+  float mom;
+  float final_mom;
+  float mult;
+};
+
+struct centerScaleParams {
+  float scale;
+  float diameter;
+};
+
 enum class ShaderBuffers {
   POSITION = 0,
   NEIGHBOUR = 1,
@@ -18,6 +54,12 @@ enum class ShaderBuffers {
   KLDIV = 10,
   NUM_POINTS = 11,
   IMAGE_WORKGROUP = 12,
+  UBO_STENCIL = 13,
+  UBO_FIELD = 14,
+  UBO_INTERP = 15,
+  UBO_FORCES = 16,
+  UBO_UPDATE = 17,
+  UBO_CENTER_SCALE = 18
 };
 
 typedef std::map<ShaderBuffers, std::shared_ptr<kp::Tensor>> TensorMap;
