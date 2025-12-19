80417b66e85e0d44bed7f0a367f9140bd84f2946
Baldur van Lew <b.van_lew@lumc.nl>
2025-12-19 16:54:36 +0100

    Revert to bounds without subgroup
    Appears more stable - need to debug subgroup implementation

--- hdi/dimensionality_reduction/gpgpu_vulkan/shaders/shader_progs.h
+++ hdi/dimensionality_reduction/gpgpu_vulkan/shaders/shader_progs.h
@@ -73,7 +73,8 @@ public:
   BoundsShaderProg(std::shared_ptr<kp::Manager> mgr, TensorMap& tensors) :
     _mgr(mgr),
     _tensors(tensors),
-    _shaderBinary(getSPIRVBinaries()[SPIRVShader::BOUNDS_SUBGROUP] )
+    //	_shaderBinary(getSPIRVBinaries()[SPIRVShader::BOUNDS_SUBGROUP] )
+    _shaderBinary(getSPIRVBinaries()[SPIRVShader::BOUNDS] )
   {}
 
   void record_padded(
