a51b41fc3a3b789bb4f00ee8bf43fdc543c5810c
Baldur van Lew <b.van_lew@lumc.nl>
2026-02-16 16:56:39 +0100

    Prepend the vcpkg toolchain to the conan toolchain

--- conanfile.py
+++ conanfile.py
@@ -145,6 +145,12 @@ class HDILibConan(ConanFile):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.set_property("skip_deps_file", True)
         self.cpp_info.set_property("cmake_config_file", True)
+        # inject the vcpkg toolchain file into the start of the conan generated file
+        vcpkg_root = os.getenv("VCPKG_INSTALLATION_ROOT", None)
+        if vcpkg_root is None:
+            raise RuntimeError("Expected a preinstalled vcpkg and the environment variable VCPKG_INSTALLATION_ROOT to be available")
+        vcpkg_tc_path = Path(vcpkg_root, "scripts", "buildsystems", "vcpkg.cmake").as_posix()
+        self.conf_info.define("tools.cmake.cmaketoolchain:user_toolchain", [vcpkg_tc_path])
 
     def package(self):
         install_dir = Path(self.build_folder).joinpath("install")
