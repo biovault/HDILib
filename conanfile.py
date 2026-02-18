d6bc4c4a0bbac270677d3179d7af5c6805ae41f8
Baldur van Lew <b.van_lew@lumc.nl>
2026-02-18 18:16:45 +0100

    Temporarily only build Release
    For development purposes

--- conanfile.py
+++ conanfile.py
@@ -165,9 +165,9 @@ class HDILibConan(ConanFile):
         install_dir = Path(self.build_folder).joinpath("install")
         install_dir.mkdir(exist_ok=True)
 
-        cmake_debug = self._configure_cmake()
-        cmake_debug.build(build_type="Debug")
-        cmake_debug.install(build_type="Debug")
+        #cmake_debug = self._configure_cmake()
+        #cmake_debug.build(build_type="Debug")
+        #cmake_debug.install(build_type="Debug")
 
         if os.getenv("Analysis", None) is None:
             # Disable code analysis in Release mode
@@ -175,9 +175,9 @@ class HDILibConan(ConanFile):
             cmake_release.build(build_type="Release")
             cmake_release.install(build_type="Release")
 
-            cmake_release = self._configure_cmake()
-            cmake_release.build(build_type="RelWithDebInfo")
-            cmake_release.install(build_type="RelWithDebInfo")
+            #cmake_release = self._configure_cmake()
+            #cmake_release.build(build_type="RelWithDebInfo")
+            #cmake_release.install(build_type="RelWithDebInfo")
 
 
     def package_id(self):
