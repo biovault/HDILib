596bb342753337ec4044837a38bf15ab99129849
Alexander Vieth <vieth.alexander@gmx.net>
2026-05-05 09:48:19 +0200

    Use random generator and namespaces

--- example/src/example.cpp
+++ example/src/example.cpp
@@ -12,39 +12,33 @@
 #include <exception>
 #include <iostream>
 #include <memory>
+#include <random>
 #include <stdexcept>
 #include <vector>
 #include <cstdint>
 
-using namespace hdi::dr;
-using namespace hdi::data;
-using namespace hdi::utils;
-
-namespace {
-    float f()
-    {
-        float res = static_cast<float>(std::rand()) / RAND_MAX;
-        return res;
-    }
-}
-
 int main(int argc, char** argv)
 {
-    try {
-        std::cout << "Create random data\n";
-        constexpr int num_points = 10000;
-        constexpr int num_dims = 100;
-        std::vector<float> data(num_points * num_dims);
-        std::cout << "Data vector created\n";        
-        std::generate(data.begin(), data.end(), f);
-        std::cout << "Data vector filled\n";
-        
-		HDJointProbabilityGenerator<float>::Parameters prob_gen_param;        
+    std::cout << "Create random data\n";
+    constexpr int num_points = 10000;
+    constexpr int num_dims = 100;
+    std::vector<float> data(num_points * num_dims);
+
+    {
+        std::mt19937 rng(std::random_device{}());
+        std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+        std::generate(data.begin(), data.end(), [&]() {
+            return dist(rng);
+            });
+    }
+
+	try {
+		hdi::dr::HDJointProbabilityGenerator<float>::Parameters prob_gen_param;
         prob_gen_param._perplexity = 30;
-        prob_gen_param._aknn_algorithm = KNN_FLANN; // choose Flann
+        prob_gen_param._aknn_algorithm = hdi::dr::KNN_FLANN; // choose Flann
         
-        HDJointProbabilityGenerator<float> prob_gen;
-		HDJointProbabilityGenerator<float>::sparse_scalar_matrix_type distributions;
+        hdi::dr::HDJointProbabilityGenerator<float> prob_gen;
+        hdi::dr::HDJointProbabilityGenerator<float>::sparse_scalar_matrix_type distributions;
         std::cout << "Compute probability distributions\n";
         prob_gen.computeProbabilityDistributions(
             data.data(),
@@ -55,15 +49,15 @@ int main(int argc, char** argv)
         std::cout << "Probability distributions created\n";    
         
         std::cout << "Create a Gradient Descent TSNE instance\n";
-        GradientDescentTSNETexture tSNE; 
-        CoutLog log;
+        hdi::dr::GradientDescentTSNETexture tSNE;
+        hdi::utils::CoutLog log;
         tSNE.setLogger(&log);
 
         auto offscreenBuffer = std::make_unique<OffscreenBufferGLFW>();
         offscreenBuffer->initialize();
         offscreenBuffer->bindContext();
 
-        TsneParameters grad_desc_params = TsneParameters();
+        auto grad_desc_params = hdi::dr::TsneParameters();
         hdi::data::Embedding<float> embedding;
 
         std::cout << "Run TSNE gradient descent\n";
