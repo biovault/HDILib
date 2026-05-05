338430819de346d56e50b0426368e1f896c66f2c
Alexander Vieth <vieth.alexander@gmx.net>
2026-05-05 09:52:25 +0200

    Refactor: Use logger consistently in example

--- example/src/example.cpp
+++ example/src/example.cpp
@@ -15,16 +15,15 @@
 #include <random>
 #include <stdexcept>
 #include <vector>
-#include <cstdint>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Create random data\n";
     constexpr int num_points = 10000;
     constexpr int num_dims = 100;
     std::vector<float> data(num_points * num_dims);
 
     {
+        std::cout << "Create random data\n";
         std::mt19937 rng(std::random_device{}());
         std::uniform_real_distribution<float> dist(0.0f, 1.0f);
         std::generate(data.begin(), data.end(), [&]() {
@@ -37,38 +36,45 @@ int main(int argc, char** argv)
         prob_gen_param._perplexity = 30;
         prob_gen_param._aknn_algorithm = hdi::dr::KNN_FLANN; // choose Flann
         
-        hdi::dr::HDJointProbabilityGenerator<float> prob_gen;
+        hdi::utils::CoutLog logger;
+
         hdi::dr::HDJointProbabilityGenerator<float>::sparse_scalar_matrix_type distributions;
-        std::cout << "Compute probability distributions\n";
-        prob_gen.computeProbabilityDistributions(
-            data.data(),
-            num_dims,
-            num_points,
-            distributions,
-            prob_gen_param);
-        std::cout << "Probability distributions created\n";    
+		{
+            std::cout << "Compute probability distributions\n";
+            hdi::dr::HDJointProbabilityGenerator<float> prob_gen;
+			prob_gen.setLogger(&logger);
+
+			prob_gen.computeProbabilityDistributions(
+				data.data(),
+				num_dims,
+				num_points,
+				distributions,
+				prob_gen_param);
+			std::cout << "Probability distributions created\n";
+		}
         
-        std::cout << "Create a Gradient Descent TSNE instance\n";
-        hdi::dr::GradientDescentTSNETexture tSNE;
-        hdi::utils::CoutLog log;
-        tSNE.setLogger(&log);
+		{
+			std::cout << "Create a Gradient Descent TSNE instance\n";
+			hdi::dr::GradientDescentTSNETexture tSNE;
+			tSNE.setLogger(&logger);
 
-        auto offscreenBuffer = std::make_unique<OffscreenBufferGLFW>();
-        offscreenBuffer->initialize();
-        offscreenBuffer->bindContext();
+			auto offscreenBuffer = std::make_unique<OffscreenBufferGLFW>();
+			offscreenBuffer->initialize();
+			offscreenBuffer->bindContext();
 
-        auto grad_desc_params = hdi::dr::TsneParameters();
-        hdi::data::Embedding<float> embedding;
+			auto grad_desc_params = hdi::dr::TsneParameters();
+			hdi::data::Embedding<float> embedding;
 
-        std::cout << "Run TSNE gradient descent\n";
-        tSNE.initialize(distributions, &embedding, grad_desc_params);
+			std::cout << "Run TSNE gradient descent\n";
+			tSNE.initialize(distributions, &embedding, grad_desc_params);
 
-        constexpr uint32_t num_iterations = 1000;
-        for (uint32_t it = 0; it < num_iterations; it++)
-            tSNE.doAnIteration();
+			constexpr uint32_t num_iterations = 1000;
+			for (uint32_t it = 0; it < num_iterations; it++)
+				tSNE.doAnIteration();
 
-        offscreenBuffer->releaseContext();
-        offscreenBuffer->destroyContext();
+			offscreenBuffer->releaseContext();
+			offscreenBuffer->destroyContext();
+		}
 
         std::cout << "Finished\n";
         std::cout << std::flush;
