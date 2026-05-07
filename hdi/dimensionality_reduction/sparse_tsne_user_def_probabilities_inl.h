779374c2309841ef5e77ff02ea7237851e94d0a9
Alexander Vieth <vieth.alexander@gmx.net>
2026-05-07 16:04:25 +0200

    remove unused flann includes

--- hdi/dimensionality_reduction/sparse_tsne_user_def_probabilities_inl.h
+++ hdi/dimensionality_reduction/sparse_tsne_user_def_probabilities_inl.h
@@ -45,24 +45,6 @@
 #include <dispatch/dispatch.h>
 #endif
 
-#pragma warning( push )
-#pragma warning( disable : 4267)
-#pragma warning( push )
-#pragma warning( disable : 4291)
-#pragma warning( push )
-#pragma warning( disable : 4996)
-#pragma warning( push )
-#pragma warning( disable : 4018)
-#pragma warning( push )
-#pragma warning( disable : 4244)
-//#define FLANN_USE_CUDA
-#include "flann/flann.h"
-#pragma warning( pop )
-#pragma warning( pop )
-#pragma warning( pop )
-#pragma warning( pop )
-#pragma warning( pop )
-
 namespace hdi{
   namespace dr{
 
