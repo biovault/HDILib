c43327a85c83c23797f4bd065f28b85b04fc0f6c
Alexander Vieth <a.vieth@lumc.nl>
2026-05-12 16:42:46 +0200

    Remove scripts folder (#75)

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Install the dependencies and build the library in ./build
-export C_INCLUDE_PATH=/usr/local/include
-export CPLUS_INCLUDE_PATH=/usr/local/include
-
-cd build
-cmake  -DCMAKE_BUILD_TYPE=Release ..
-make -j 8
