dcc7aaf425ed116d4dae855f79380381defa15c9
Baldur van Lew <baldurvanlew@gmail.com>
2026-08-17 16:14:58 +0200

    Use the defaultDispatchLoaderDynamic in createBuffer as this should be initialized

--- hdi/dimensionality_reduction/gpgpu_vulkan/shaders/vkUniformBufferHelper.h
+++ hdi/dimensionality_reduction/gpgpu_vulkan/shaders/vkUniformBufferHelper.h
@@ -83,7 +83,7 @@ private:
     };
   
     // vk::Result res = mVkDevice->createBuffer(&bufferInfo, nullptr, &mVkBuffer);
-    mVkBuffer = mVkDevice->createBuffer(bufferInfo, nullptr, VULKAN_HPP_DEFAULT_DISPATCHER);
+    mVkBuffer = mVkDevice->createBuffer(bufferInfo, nullptr, vk::detail::defaultLoaderDynamic);
     //if (res != vk::Result::eSuccess) {
     //    throw std::runtime_error("Failed to create UBO buffer!");
     //}
