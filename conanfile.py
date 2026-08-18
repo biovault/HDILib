3ec86a3eb1f73c8fcbfaadb2caf3be51ad8202bf
Baldur van Lew <baldurvanlew@gmail.com>
2026-08-18 16:17:56 +0200

    Fix NDEBUG in release build of kompute to match hdilib

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ required_conan_version = "~=1.66.0"
 
 class HDILibConan(ConanFile):
     name = "HDILib"
-    version = "2.0.3a8"
+    version = "2.0.3a9"
     description = (
         "HDILib is a library for the scalable analysis of large and high-dimensional"
         " data. "
