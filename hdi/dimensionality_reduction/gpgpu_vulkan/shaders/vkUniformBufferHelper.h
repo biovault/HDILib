d672c82c829596a6bc3a0096fb3938addcef1cfd
Baldur van Lew <baldurvanlew@gmail.com>
2026-08-18 10:22:47 +0200

    Fix loader name

--- hdi/dimensionality_reduction/gpgpu_vulkan/shaders/vkUniformBufferHelper.h
+++ hdi/dimensionality_reduction/gpgpu_vulkan/shaders/vkUniformBufferHelper.h
@@ -83,7 +83,7 @@ private:
     };
   
     // vk::Result res = mVkDevice->createBuffer(&bufferInfo, nullptr, &mVkBuffer);
-    mVkBuffer = mVkDevice->createBuffer(bufferInfo, nullptr, defaultLoaderDynamic);
+    mVkBuffer = mVkDevice->createBuffer(bufferInfo, nullptr, vk::detail::defaultDispatchLoaderDynamic);
     //if (res != vk::Result::eSuccess) {
     //    throw std::runtime_error("Failed to create UBO buffer!");
     //}
